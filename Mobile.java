92c920c7ad8bef590c543708586ff1904a3b68c1
Manothini333 <124478549+Manothini333@users.noreply.github.com>
2023-02-25 13:56:05 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileBrandName = "Redmi";
+		char mobileLogo = '@';
+		short noOfMobilePiece = 1234;
+		int modelNumber = 123;
+		long mobileImeiNumber = 1234567898765l;
+		float mobilePrice = 14999.5f;
+		boolean isDamaged = false; 
+		System.out.println("Mobile name is " + mobileBrandName);
+		System.out.println("Mobile logo is " + mobileLogo);
+		System.out.println("Number of mobile pieces are " + noOfMobilePiece);
+		System.out.println("Mobile model number is " + modelNumber);
+		System.out.println("Mobile imei number is " + mobileImeiNumber);
+		System.out.println("Mobile price is " + mobilePrice);
+		System.out.println("Is mobile got damaged " + isDamaged);
+	}
+
+}
